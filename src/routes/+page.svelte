87122a65e497ad5c0af16cf366f1060cef8a9141
Malik Zubayer <zubayer@cloudly.io>
2025-10-08 12:54:44 +0600

    feat: about section

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,8 +2,10 @@
 	import SummaryBlock from '$lib/blocks/SummaryBlock.svelte';
 	import ProjectBlock from '$lib/blocks/ProjectBlock.svelte';
 	import IdeaBlock from '$lib/blocks/IdeaBlock.svelte';
+	import AboutBlock from '$lib/blocks/AboutBlock.svelte';
 </script>
 
 <SummaryBlock />
 <ProjectBlock />
 <IdeaBlock />
+<AboutBlock />
