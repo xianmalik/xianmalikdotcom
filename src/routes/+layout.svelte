7d8a0ef8bb8d620273fa793927ba48e253478cec
Malik Zubayer Ul Haider <mzubayeruh@gmail.com>
2025-11-10 14:26:40 +0600

    lenis.init()

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,8 +1,40 @@
 <script lang="ts">
 	import '../app.css';
 	import favicon from '$lib/assets/favicon.svg';
+	import Lenis from 'lenis';
+	import { onMount } from 'svelte';
+	import gsap from 'gsap';
+	import { ScrollTrigger } from 'gsap/ScrollTrigger';
 
 	let { children } = $props();
+
+	onMount(() => {
+		// Register GSAP ScrollTrigger plugin
+		gsap.registerPlugin(ScrollTrigger);
+
+		const lenis = new Lenis({
+			duration: 1.2,
+			easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
+			orientation: 'vertical',
+			smoothWheel: true,
+		});
+
+		// Integrate Lenis with GSAP ScrollTrigger
+		lenis.on('scroll', ScrollTrigger.update);
+
+		gsap.ticker.add((time) => {
+			lenis.raf(time * 1000);
+		});
+
+		gsap.ticker.lagSmoothing(0);
+
+		return () => {
+			lenis.destroy();
+			gsap.ticker.remove((time) => {
+				lenis.raf(time * 1000);
+			});
+		};
+	});
 </script>
 
 <svelte:head>
